7389290b118f1ef99a0e733d55b7a9e6daabffab
AP <aristophynes@outlook.com>
2023-01-21 02:33:21 +0100

    Update Noot.cs
    Noted a missing bracket in mockup-part

--- Verzamelde Onderdelen Classes/Noot.cs
+++ Verzamelde Onderdelen Classes/Noot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GuitareokeConsole.CollectieBeschikbareNoten
+namespace GuitareokeConsole.CollectieBeschikbareNoten{
     class Noot
     {
         string[] CollectieBeschikbareNoten = { "C", "Db", "C#", "D", "Eb", "D#", "E", "F", "Gb", "F#", "G", "Ab", "G#", "A", "A#", "Bb", "B" };
